ef0c835f570e7be9edc39ee88dbc86546fca71a6
Qnokka <106862472+Qnokka@users.noreply.github.com>
2025-05-29 14:48:09 +0900

    Feat:   Model(prisma) 기반 DTO, Entity 생성

--- backend/src/mates/dto/create-mate.dto.ts
+++ backend/src/mates/dto/create-mate.dto.ts
@@ -1,1 +1,9 @@
-export class CreateMateDto {}
+import { IsInt } from 'class-validator';
+
+export class CreateMateDto {
+  @IsInt()
+  userId: number;
+
+  @IsInt()
+  petId: number;
+}
--- backend/src/mates/entities/mate.entity.ts
+++ backend/src/mates/entities/mate.entity.ts
@@ -1,1 +1,15 @@
-export class Mate {}
+import { PetEntity } from 'src/pets/entities/pet.entity';
+
+export class MateEntity {
+  userId: number;
+  petId: number;
+  pet: PetEntity;
+
+  constructor(partial: Partial<MateEntity>) {
+    Object.assign(this, partial);
+
+    if (partial?.pet) {
+      this.pet = new PetEntity(partial.pet);
+    }
+  }
+}
